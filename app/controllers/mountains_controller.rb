ec3dafdfc9d05bd0e75217a36e3f65571c6a330b
Jamie Parsons <jhelenparsons@gmail.com>
2017-06-10 12:20:10 -0600

    mountain controller

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,10 +1,53 @@
-class MountainsController < ApplicationController
-  def index
-  end
 
-  def new
+class MountainsController < ApplicationController
+  before_action :set_mountain, except: [:create, :new, :index]
+
+  def index
+    @mountains = Mountain.all
   end
 
   def show
+  end
+
+  def new
+    @mountain = Mountain.new
+  end
+
+  def edit
+  end
+
+  def update
+
+    if Mountain.update(mountain_params)
+      redirect_to mountains_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @mountain = Mountain.new(mountain_params)
+
+    if @mountain.save
+      redirect_to mountains_path
+    else 
+      render :new
+    end
+  end
+
+  def destroy
+    @mountain.destroy
+    flash[:notice] = 'Mountain Deleted!'
+    redirect_to mountains_path
+  end
+
+  private
+
+  def mountain_params
+    params.require(:mountain).permit(:name, :elevation, :latitude, :longitude, :rating)
+  end
+
+  def set_mountain
+    @mountain = Mountain.find(params[:id])
   end
 end
