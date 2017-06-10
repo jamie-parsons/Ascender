540a37fce0890380a1805d8e393dd3c832fdb413
Jamie Parsons <jhelenparsons@gmail.com>
2017-06-10 16:38:39 -0600

    heroku commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
-    
+    @mountains = Mountain.all
+    @users_mountains = []
+
+    @mountains.each do |mountain|
+      if mountain.ability == current_user.ability
+        @users_mountains << mountain
+      end
+    end
   end
 end
