28448436e31fc4cbbe26663acfc9c39224e966fc
Jamie Parsons <jhelenparsons@gmail.com>
2017-06-10 15:03:45 -0600

    commits

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,2 +1,4 @@
 class Mountain < ApplicationRecord
+    # validates :ability, inclusion: { in: %w(beginner intermediate advanced),
+    # message: "%{value} is not valid, please enter: beginner, intermediate, advanced" }
 end
--- db/migrate/20170610162711_create_mountains.rb
+++ db/migrate/20170610162711_create_mountains.rb
@@ -1,11 +1,11 @@
 class CreateMountains < ActiveRecord::Migration[5.1]
   def change
     create_table :mountains do |t|
-      t.string :name
-      t.integer :elevation
-      t.float :latitude
-      t.float :longitude
-      t.string :ability
+      t.string :name, null: false
+      t.integer :elevation, null: false
+      t.float :latitude, null: false
+      t.float :longitude, null: false
+      t.string :ability, null: false
 
       t.timestamps
     end
