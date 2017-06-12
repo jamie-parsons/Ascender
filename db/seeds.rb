858d2e2112f487d3d32c9a684abb351dfc9733a0
Jamie Parsons <jhelenparsons@gmail.com>
2017-06-12 17:04:24 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,34 @@
 Mountain.create(
-        name: "King's Peak", elevation: 11000, latitude: 28.988056, 
-        longitude: 89.925278, ability: "Intermediate"
+    name: "King's Peak", elevation: 13534, latitude: 40.776382, 
+    longitude: 110.372815, ability: "Intermediate"
 )
 
-# Mountain.create(
-#         name: "Everest", elevation: 29029, latitude: 27.988056, 
-#         longitude: 86.925278, ability: "Advanced"
-# )
+Mountain.create(
+   name: "Everest", elevation: 29029, latitude: 27.988056, 
+   longitude: 86.925278, ability: "Advanced"
+)
 
+Mountain.create(
+    name: "K2", elevation: 28251, 
+    latitude: 35.8825, longitude: 76.513333, ability: "Advanced"
+)
+
+Mountain.create(
+  name: "Denali", elevation: 20310, 
+  latitude: 63.0695, longitude: -151.0074, ability: "Intermediate"
+)
+
+Mountain.create(
+  name: "Denali", elevation: 20310, 
+  latitude: 63.0695, longitude: -151.0074, ability: "Intermediate"
+)
+
+Mountain.create(
+    name: "Mount Rainier", elevation: 14411,
+    latitude: 46.852886, longitude: -121.760374, ability: "Intermediate"
+)
+
+Mountain.create(
+    name: "Annapurna IV", elevation: 24688, 
+    latitude: 28.5375, longitude: 84.082778, ability: "Advanced"
+)
