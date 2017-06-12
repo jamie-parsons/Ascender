d06475eeda3562a40cc680f0ef95058a66df38ff
Jamie Parsons <jhelenparsons@gmail.com>
2017-06-12 16:57:45 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Mountain.create(
+        name: "King's Peak", elevation: 11000, latitude: 28.988056, 
+        longitude: 89.925278, ability: "Intermediate"
+)
+
+# Mountain.create(
+#         name: "Everest", elevation: 29029, latitude: 27.988056, 
+#         longitude: 86.925278, ability: "Advanced"
+# )
+
