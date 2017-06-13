210578959d24e65890c1568ad9ce184a443fc1af
Jamie Parsons <jhelenparsons@gmail.com>
2017-06-12 18:07:46 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,6 @@ Mountain.create(
 )
 
 Mountain.create(
-  name: "Denali", elevation: 20310, 
-  latitude: 63.0695, longitude: -151.0074, ability: "Intermediate"
-)
-
-Mountain.create(
     name: "Mount Rainier", elevation: 14411,
     latitude: 46.852886, longitude: -121.760374, ability: "Intermediate"
 )
@@ -31,4 +26,24 @@ Mountain.create(
 Mountain.create(
     name: "Annapurna IV", elevation: 24688, 
     latitude: 28.5375, longitude: 84.082778, ability: "Advanced"
+)
+
+Mountain.create(
+    name: "Mount Peale", elevation: 12721, 
+    latitude: 38.438561, longitude:	109.229179, ability: "Intermediate"
+)
+
+Mountain.create(
+    name: "Strawberry Peak", elevation:	10335, 
+    latitude: 40.047061, longitude: 110.984673, ability: "Beginner"
+)
+
+Mountain.create(
+    name: "Keg Mountain" elevation:	7305, 
+    latitude: 39.788982, longitude:	112.8413, ability: "Beginner"
+)
+
+Mountain.create(
+    name: "Hilgard Mountain" elevation:	11533, 
+    latitude: 38.684123, longitude: 111.543821, ability: "Beginner"
 )
